ad3da2d26bb68729d924c9fec6f7971fbc94ebc3
Osama92 <try2rym@yahoo.com>
2024-08-27 22:19:08 +0100

    Minor changes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button, TouchableOpacity, Alert } from 'react-native';
+import { Text, View, StyleSheet, Button} from 'react-native';
 import {CameraView, Camera} from  'expo-camera'
 
 
