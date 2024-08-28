0c05238b7bc5be65dab27a3fa13ca401d85e2b6b
Osama92 <try2rym@yahoo.com>
2024-08-28 16:14:56 +0100

    changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { registerRootComponent } from 'expo';
 
-import App from './App';
+import App from './_layouts';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(_layouts);
